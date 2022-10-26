3ecb77635b2c6bba075517246dbf65ee88d5ea9e
Roma-coder <56150485+Roma-coder@users.noreply.github.com>
2022-10-26 20:55:27 +0300

    calendar_event_fix

---
+++ RehabCV/ViewModels/EventViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using RehabCV.Models;
+using System.Collections.Generic;
+
+
+namespace RehabCV.ViewModels
+{
+    public class EventViewModel
+    {
+        public List<SelectListItem> TherapistList { get; set; }
+        public List<SelectListItem> ChildList { get; set; }
+    }
+}
