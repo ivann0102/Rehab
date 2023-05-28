a42dfbd0e7d50bea5d9dc38aa639e9f2d9abb2df
Ivan <nikolaiesku.ivan@gmail.com>
2023-05-28 10:48:57 +0300

    Fixed timestamp without timezone error

--- RehabCV/Startup.cs
+++ RehabCV/Startup.cs
@@ -66,6 +66,8 @@ namespace RehabCV
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
         {
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
